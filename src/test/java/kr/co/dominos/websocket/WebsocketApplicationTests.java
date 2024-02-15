4405872223e379f00b9ffe16537bd91ddf2c680b
wrlee <wrlee@chonf.co.kr>
2024-02-15 15:49:26 +0900

    initial commit

---
+++ src/test/java/kr/co/dominos/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,11 @@
+package kr.co.dominos.websocket;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class WebsocketApplicationTests {
+
+
+
+}
